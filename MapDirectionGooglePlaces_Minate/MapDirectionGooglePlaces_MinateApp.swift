32f6a09ffa3c6dc051e784347bb484ae5d84327d
MinateTina <tinatung509@gmail.com>
2022-09-23 13:54:49 -0700

    Initial Commit

---
+++ MapDirectionGooglePlaces_Minate/MapDirectionGooglePlaces_MinateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapDirectionGooglePlaces_MinateApp.swift
+//  MapDirectionGooglePlaces_Minate
+//
+//  Created by Minate on 9/23/22.
+//
+
+import SwiftUI
+
+@main
+struct MapDirectionGooglePlaces_MinateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
